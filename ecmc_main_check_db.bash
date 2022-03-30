7779f670d2e4f830bc2c5fb10af7d09e94f2c9a1
Anders Sandstrom <anders.sandstrom@esss.se>
2022-03-30 11:48:18 +0200

    WIP

--- ecmc_main_check_db.bash
+++ ecmc_main_check_db.bash
@@ -20,4 +20,4 @@ length=$(expr length $prefix)
 # remove prefix for length check
 property=$(cat pvs.log | cut -c $length- | cut -c 3-)
 
-echo "$property" | awk -v maxl=$maxproplength '{len=length($1); if(len>maxl) {print $0 ": To long (" len "chars)";}}'
+echo "$property" | awk -v maxl=$maxproplength '{len=length($1); if(len>maxl) {print $0 ": PV property to long (" len " > " maxl" chars)";}}'
